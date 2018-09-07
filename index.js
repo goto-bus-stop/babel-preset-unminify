7dbd282a11e2dbe3cd52453f137cd5752b774db7
Renée Kooi <renee@kooi.me>
2018-09-07 19:23:48 +0200

    Use Object.assign

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 var d = require('defined')
-var xtend = require('xtend/mutable')
 var prettier = require('babel-plugin-generator-prettier')
 var phonetize = require('babel-plugin-transform-phonetize')
 var beautifier = require('babel-plugin-transform-beautifier')
@@ -7,7 +6,7 @@ var beautifier = require('babel-plugin-transform-beautifier')
 function prettierOptions () {
   return {
     manipulateOptions: function (opts) {
-      xtend(opts.generatorOpts, {
+      Object.assign(opts.generatorOpts, {
         semi: false,
         printWidth: 120,
         singleQuote: true
